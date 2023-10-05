4aad781e24bf45ae7c03cee6a2bf8c3d709fb43b
awdwadwa213eawdaw <142877519+awdwadwa213eawdaw@users.noreply.github.com>
2023-10-05 15:31:36 +0200

    Created 757162BF-29C9-4A12-98A3-3C9AA8FCC0CA_S.lua

---
+++ 757162BF-29C9-4A12-98A3-3C9AA8FCC0CA_S.lua
@@ -0,0 +1,47 @@
+local plate = script.Parent
+while true do
+	wait(.7)
+	plate.Reflectance = .05
+	wait(.03)
+	plate.Reflectance = .1
+	wait(.01)
+	plate.Reflectance = .2
+	wait(.01)
+	plate.Reflectance = .3
+	wait(.02)
+	plate.Reflectance = .4
+	wait(.01)
+	plate.Reflectance = .5
+	wait(.01)
+	plate.Reflectance = .6
+	wait(.01)
+	plate.Reflectance = .7
+	wait(.01)
+	plate.Reflectance = .8
+	wait(.01)
+	plate.Reflectance = .9
+	wait(.01)
+	plate.Reflectance = 1
+	wait(.3)
+	plate.Reflectance = .9
+	wait(.08)
+	plate.Reflectance = .8
+	wait(.01)
+	plate.Reflectance = .7
+	wait(.01)
+	plate.Reflectance = .6
+	wait(.01)
+	plate.Reflectance = .5
+	wait(.03)
+	plate.Reflectance = .4
+	wait(.01)
+	plate.Reflectance = .3
+	wait(.04)
+	plate.Reflectance = .2
+	wait(.02)
+	plate.Reflectance = .1
+	wait(.01)
+	plate.Reflectance = .05
+	wait(.01)
+	plate.Reflectance = 0
+end
