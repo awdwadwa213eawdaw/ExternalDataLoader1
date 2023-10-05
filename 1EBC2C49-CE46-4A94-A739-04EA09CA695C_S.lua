7ecd8cb73645079308f21bbfdf98e8447a36e888
awdwadwa213eawdaw <142877519+awdwadwa213eawdaw@users.noreply.github.com>
2023-10-05 15:30:54 +0200

    Created 1EBC2C49-CE46-4A94-A739-04EA09CA695C_S.lua

---
+++ 1EBC2C49-CE46-4A94-A739-04EA09CA695C_S.lua
@@ -0,0 +1,43 @@
+while true do
+	wait(.2)
+	script.Parent.Range = script.Parent.Range + 1
+	wait(.03)
+	script.Parent.Range = script.Parent.Range + 1
+	wait(.01)
+	script.Parent.Range = script.Parent.Range + 1
+	wait(.01)
+	script.Parent.Range = script.Parent.Range + 1
+	wait(.02)
+	script.Parent.Range = script.Parent.Range + 1
+	wait(.01)
+	script.Parent.Range = script.Parent.Range + 1
+	wait(.01)
+	script.Parent.Range = script.Parent.Range + 1
+	wait(.01)
+	script.Parent.Range = script.Parent.Range + 1
+	wait(.01)
+	script.Parent.Range = script.Parent.Range + 1
+	wait(.01)
+	script.Parent.Range = script.Parent.Range + 1
+	wait(.4)
+	script.Parent.Range = script.Parent.Range - 1
+	wait(.08)
+	script.Parent.Range = script.Parent.Range - 1
+	wait(.01)
+	script.Parent.Range = script.Parent.Range - 1
+	wait(.01)
+	script.Parent.Range = script.Parent.Range - 1
+	wait(.01)
+	script.Parent.Range = script.Parent.Range - 1
+	wait(.03)
+	script.Parent.Range = script.Parent.Range - 1
+	wait(.01)
+	script.Parent.Range = script.Parent.Range - 1
+	wait(.04)
+	script.Parent.Range = script.Parent.Range - 1
+	wait(.02)
+	script.Parent.Range = script.Parent.Range - 1
+	wait(.01)
+	script.Parent.Range = script.Parent.Range - 1
+	wait(.01)
+end
